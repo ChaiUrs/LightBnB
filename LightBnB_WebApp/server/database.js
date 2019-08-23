f48b5faa5c227d164c8523bd533cfe8b3fd7c571
ChaiUrs <mysorechaitra@gmail.com>
2019-08-23 00:59:31 +0000

    Updated with add property function to the properties table

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -196,11 +196,9 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, 
-    city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
   const queryString = `
-  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, 
-    city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *;
   `;
